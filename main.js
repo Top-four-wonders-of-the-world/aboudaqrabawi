77b74be090d57d6b26c93856a331f083f78ab3a0
aboudaqrabawi <aqrabawijoe@gmail.com>
2020-10-04 12:01:37 +0300

    project

---
+++ main.js
@@ -0,0 +1,4 @@
+$(".seven-wonders").append('<div class="Petra1">Petra</div>')
+$(".Petra1").append('<br>')
+
+$(".Petra1").append('<img id="image-petra" src="file:///C:/Users/aqrab/OneDrive/Desktop/petra.jfif"/>')
